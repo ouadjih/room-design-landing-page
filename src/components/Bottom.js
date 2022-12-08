5efe25609dba24c49848c04edd4af47f1a107cf2
ouadjih <47463757+ouadjih@users.noreply.github.com>
2022-12-08 12:32:44 +0100

    added some styling for each components

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import './Bottom.css'
+import bottomImgDark from '../asset/images/image-about-dark.jpg'
+import bottomImgLight from '../asset/images/image-about-light.jpg'
 export const Bottom = () => {
   return (
-    <div>Bottom</div>
+    <div class="bottom">
+      <img src={bottomImgDark} alt="darkimg" class="bottom-one"/>
+      <div class="bottom-two">
+        <div class="bottom-two-title">About our furniture</div>
+        <div class="bottom-two-description">
+          Our multifunctional collection blends design and function to suit your
+          individual taste. Make each room unique, or pick a cohesive theme that
+          best express your interests and what inspires you. Find the furniture
+          pieces you need, from traditional to contemporary styles or anything
+          in between. Product specialists are available to help you create your
+          dream space.
+        </div>
+      </div>
+      <img src={bottomImgLight} alt="lightimg" class="bottom-three"/>
+    </div>
   )
 }
--- src/components/Top.js
+++ src/components/Top.js
@@ -1,7 +1,30 @@
 import React from 'react'
 import './Top.css'
+import firstImg from '../asset/images/desktop-image-hero-1.jpg'
+import arrowLeft from '../asset/images/icon-angle-left.svg'
+import arrowRight from '../asset/images/icon-angle-right.svg'
+import iconArrow from '../asset/images/icon-arrow.svg'
 export const Top = () => {
   return (
-    <div>Top</div>
+    <div className="top">
+      <img className="top-left" src={firstImg} alt="hero" />
+      <div className="top-right">
+        <div className="top-right-top">
+          <div className="top-right-title">Discover innovative ways to decorate</div>
+        <div className="top-right-description">
+          We provide unmatched quality, comfort, and style for property owners
+          across the country. Our experts combine form and function in bringing
+          your vision to life. Create a room in your own style with our
+          collection and make your property a reflection of you and what you
+          love.
+        </div>
+        <a className="top-right-btn" href="/">Shop now <img src={iconArrow} alt="arrow"/></a>
+        </div>
+        <div className="btn">
+          <img className="btn-left" src={arrowLeft} alt="btn-left" />
+          <img className="btn-right" src={arrowRight} alt="btn-right" />
+        </div>
+      </div>
+    </div>
   )
 }
